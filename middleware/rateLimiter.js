d1858c0a1bba9f862e7dec07e38d6b157e886ea2
Deepak Maurya <deepakm@zignuts.com>
2024-05-23 16:54:04 +0530

    added redis client

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,4 +1,5 @@
 const setRateLimit = require("express-rate-limit");
+const redis = require('../redis-client');
 
 // Rate limit middleware
 const rateLimitMiddleware = setRateLimit({
@@ -8,4 +9,30 @@ const rateLimitMiddleware = setRateLimit({
   headers: true,
 });
 
-module.exports = { rateLimitMiddleware };
+// Rate limit middleware for API using redis
+const redisRateLimitMiddleware = function({ windowMs, maxReq }) {
+  return async function (req, res, next) {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const requests = await redis.incr(ip);
+    console.log(`number of requests made by ${ip} is ${requests}`);
+
+    let ttl
+    if (requests === 1) {
+      await redis.expire(ip, windowMs);
+      ttl = 60;
+    } else {
+      ttl = await redis.ttl(ip);
+    }
+    if (requests > maxReq) {
+      return res.status(429).json({
+        message: "Too many requests",
+        ttl: ttl
+      });
+    } else {
+      req.ttl = ttl;
+      return next();
+    }
+  }
+}
+
+module.exports = { rateLimitMiddleware, redisRateLimitMiddleware };
